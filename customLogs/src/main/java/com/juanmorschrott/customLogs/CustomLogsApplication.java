410cd62f37f6591fef78341f37f636b12b770d10
jumoreno <jmoreno.ext@hotelbeds.com>
2019-06-07 10:42:08 +0200

    Added custom logs tutorial

---
+++ customLogs/src/main/java/com/juanmorschrott/customLogs/CustomLogsApplication.java
@@ -0,0 +1,13 @@
+package com.juanmorschrott.customLogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomLogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomLogsApplication.class, args);
+	}
+
+}
