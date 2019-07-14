74c3f12a4e787674d89838ebf6546c2763fcafd7
Juan Andres Moreno Schrott <redtitle@msn.com>
2019-07-14 09:56:27 +0200

    Added pagination

--- openTracing/hotel/src/main/java/com/juanmorschrott/hotel/controller/HotelController.java
+++ openTracing/hotel/src/main/java/com/juanmorschrott/hotel/controller/HotelController.java
@@ -3,34 +3,56 @@ package com.juanmorschrott.hotel.controller;
 import com.juanmorschrott.hotel.model.Hotel;
 import com.juanmorschrott.hotel.repository.HotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/hotels")
 public class HotelController {
 
     @Autowired
     private HotelRepository hotelRepository;
 
-    @GetMapping("/hotels")
+    @GetMapping()
     public ResponseEntity findAll() {
 
         List<Hotel> hotels = (List<Hotel>) hotelRepository.findAll();
-        ResponseEntity response;
 
         if (!hotels.isEmpty()) {
-            response = new ResponseEntity<>(hotels, HttpStatus.OK);
+            return new ResponseEntity<>(hotels, HttpStatus.OK);
         } else {
-            response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping()
+    public ResponseEntity findByCity(@RequestParam("city") String city,
+                                     @RequestParam("start") Integer start,
+                                     @RequestParam("limit") Integer limit) {
+
+        if (city == null || start == null || limit == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        return response;
+        Pageable firstPage = PageRequest.of(start, limit);
+
+        Page<Hotel> hotels = hotelRepository.findByCity(city, firstPage);
+
+        if (!hotels.isEmpty()) {
+            return new ResponseEntity<>(hotels.getContent(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
--- openTracing/hotel/src/main/java/com/juanmorschrott/hotel/repository/HotelRepository.java
+++ openTracing/hotel/src/main/java/com/juanmorschrott/hotel/repository/HotelRepository.java
@@ -1,8 +1,13 @@
 package com.juanmorschrott.hotel.repository;
 
 import com.juanmorschrott.hotel.model.Hotel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface HotelRepository extends CrudRepository<Hotel, String> {
+
+    Page<Hotel> findByCity(String city, Pageable pageable);
 
 }
