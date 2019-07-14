fb7073fedc305fb405655563dc6f786d6576aa65
Juan Andres Moreno Schrott <redtitle@msn.com>
2019-07-14 09:06:59 +0200

    Refactor

--- openTracing/hotel/src/main/java/com/juanmorschrott/hotel/repository/HotelRepository.java
+++ openTracing/hotel/src/main/java/com/juanmorschrott/hotel/repository/HotelRepository.java
@@ -3,5 +3,6 @@ package com.juanmorschrott.hotel.repository;
 import com.juanmorschrott.hotel.model.Hotel;
 import org.springframework.data.repository.CrudRepository;
 
-public interface HotelRepository extends CrudRepository<Hotel, Long> {
+public interface HotelRepository extends CrudRepository<Hotel, String> {
+
 }
